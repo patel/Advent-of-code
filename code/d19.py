eedd74769e8e1156a7baac2397c83ce2ea7f95ba
Krunal Patel <krunal@sevenrooms.com>
2016-02-13 13:23:03 -0500

    Day 19 part 2

--- code/d19.py
+++ code/d19.py
@@ -1,9 +1,15 @@
 import re
 from collections import defaultdict
-import pprint
+
+class Node:
+    def __init__(self, source, count, parent):
+        self.source = source
+        self.parent = parent
+        self.count = count
+        self.children = {}
 
 def generate_map_from_replacement_rules(rules):
-    expression_map =  defaultdict(list)
+    expression_map = defaultdict(list)
     reverse_rule_expression = defaultdict(list)
     rule_expression = re.compile('(.+) => (.+)')
     for rule in rules:
@@ -12,6 +18,7 @@ def generate_map_from_replacement_rules(rules):
         expression_map[k].append(v)
         reverse_rule_expression[v].append(k)
     return expression_map, reverse_rule_expression
+
 
 def get_num_distinct_molecules(rules_str, target_molecule):
     expression_map, reverse_rule_expression = generate_map_from_replacement_rules(rules_str.split('\n'))
@@ -26,39 +33,43 @@ def get_num_distinct_molecules(rules_str, target_molecule):
     for m, locations in target_molecules_lookup.iteritems():
         for location in locations:
             for reduction in expression_map[m]:
-                result.append(target_molecule[:location] + reduction + target_molecule[location+len(m):])
+                result.append(target_molecule[:location] + reduction + target_molecule[location + len(m):])
     return len(set(result))
 
 
-def get_min_num_steps(rules_str, target_molecule, source_molecule):
+def get_min_num_steps(rules_str, start_molecule, target_molecule):
     expression_map, reverse_rule_expression = generate_map_from_replacement_rules(rules_str.split('\n'))
-    all_molecules = sorted(reverse_rule_expression.keys(), key=lambda x: -len(x))
-    current_count = 0
-    target_tree = {'target': target_molecule, 'source': '', 'prev': None, 'count': 0}
-    current_node = target_tree
-    while target_molecule != source_molecule:
+    all_molecules = sorted(reverse_rule_expression.keys())
+    all_molecules = sorted(all_molecules, key=lambda x: -len(x))
+    skipped_list = set([]) # Skip list to avoid dead ends
+    largest_reduction = len(all_molecules[0]) - len(reverse_rule_expression[all_molecules[0][0]])
+    current_node = Node(start_molecule, 0, None)
+    while current_node.source != target_molecule and current_node is not None:
         not_found = True
-        for molecule in all_molecules:
-            if not_found and \
-                target_molecule.find(molecule) >= 0 and \
-                    current_node.get('source', None) != molecule:
-                print molecule
-                target_molecule = target_molecule.replace(molecule, reverse_rule_expression[molecule][0], 1)
-                current_node['source'] = molecule
-                current_node['next'] = {'target': target_molecule, 'count': current_node.get('count', 0) + 1}
-                prev_node = current_node
-                current_node = current_node['next']
-                current_node['prev'] = prev_node
-                not_found = False
-                current_count = current_node['count']
-                continue
+        for molecule in set(all_molecules) - set(current_node.children.keys()):
+            if not_found and current_node.source.find(molecule) >= 0:
+                for r_molecule in reverse_rule_expression[molecule]:
+                    if not_found and not current_node.children.has_key(molecule + '|' + r_molecule):
+                        current_node.children[molecule + '|' + r_molecule] = \
+                            Node(current_node.source.replace(molecule, r_molecule, 1),
+                                                              current_node.count + 1,
+                                                              current_node)
+
+                        if not current_node.source.replace(molecule, r_molecule, 1) in skipped_list:
+                            current_node = current_node.children[molecule + '|' + r_molecule]
+                            not_found = False
+                        break
         if not_found:
-            target_molecule = current_node['target']
-            current_node = current_node['prev']
+            skipped_list.add(current_node.source)
+            total_length = len(current_node.source)
+            # We can backtrack the following number of steps with certainty to skip deadends
+            for i in range(0, total_length/largest_reduction - 1):
+                skipped_list.add(current_node.source)
+                current_node = current_node.parent
+            while current_node.source in skipped_list:
+                current_node = current_node.parent
 
-    pprint.pprint(current_node)
-    return current_count
-
+    return current_node.count
 
 
 t = \
@@ -106,14 +117,7 @@ e => HF
 e => NAl
 e => OMg"""
 
-z = \
-"""e => H
-e => O
-H => HO
-H => OH
-O => HH"""
 
-target_molecule = 'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'
-target_molecule_1 = 'HOHOHO'
-#print get_num_distinct_molecules(t, target_molecule)
+target_molecule = 'ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF'
+print get_num_distinct_molecules(t, target_molecule)
 print get_min_num_steps(t, target_molecule, 'e')
